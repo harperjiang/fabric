d7e163cc7a61a572d6fff416e781e23cd5220652
Hao Jiang <harperjiang@msn.com>
2022-04-20 14:25:06 -0400

    Role change to in memory WalletStore

--- client_app/src/main/java/loader/Role.java
+++ client_app/src/main/java/loader/Role.java
@@ -2,6 +2,8 @@ package loader;
 
 import org.hyperledger.fabric.gateway.Wallet;
 import org.hyperledger.fabric.gateway.Wallets;
+import org.hyperledger.fabric.gateway.impl.identity.InMemoryWalletStore;
+import org.hyperledger.fabric.gateway.spi.WalletStore;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -12,13 +14,24 @@ public abstract class Role {
 
     private Wallet wallet;
 
-    public Role(String username, String walletPath) {
+    public Role(String username) {
         this.username = username;
         try {
-            this.wallet = Wallets.newFileSystemWallet(Path.of(walletPath));
-        } catch (IOException e) {
+            this.wallet = Wallets.newWallet(createStore());
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    protected static String[] IDS = {"User1@org1.example.com", "User1@org2.example.com"};
+
+    protected static WalletStore createStore() throws IOException {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        WalletStore store = new InMemoryWalletStore();
+        for (String id : IDS) {
+            store.put(id, loader.getResourceAsStream("wallet/" + id + ".id"));
+        }
+        return store;
     }
 
     public Wallet getWallet() {
@@ -31,13 +44,13 @@ public abstract class Role {
 
     public static class Digibank extends Role {
         public Digibank() {
-            super("User1@org1.example.com", "wallet");
+            super("User1@org1.example.com");
         }
     }
 
     public static class MagnetoCorp extends Role {
         public MagnetoCorp() {
-            super("User1@org2.example.com", "wallet");
+            super("User1@org2.example.com");
         }
     }
 
