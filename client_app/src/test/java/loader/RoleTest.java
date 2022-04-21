b0192bffe67d66b6da8761c430a4f37b858b5836
harper <harperjiang@msn.com>
2022-04-21 03:40:42 +0000

    Add Execution Entry

---
+++ client_app/src/test/java/loader/RoleTest.java
@@ -0,0 +1,22 @@
+package loader;
+
+import org.hyperledger.fabric.gateway.Identity;
+import org.hyperledger.fabric.gateway.Wallet;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleTest {
+
+    @Test
+    public void testCreate() throws IOException {
+        Role role = new Role.Digibank();
+        assertNotNull(role.getWallet());
+        Wallet wallet = role.getWallet();
+
+        Identity id = wallet.get("User1@org1.example.com");
+        assertEquals("Org1MSP", id.getMspId());
+    }
+}
