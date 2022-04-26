180aee92222e895c7e84b0aca756845ba50bb6bb
Ubuntu <harper@fabric1.nmes3urj0jaedlfldcn4ay4g3f.bx.internal.cloudapp.net>
2022-04-26 21:21:24 +0000

    Add crete shell

--- couchdb_logger/src/main/shell/create.sh
+++ couchdb_logger/src/main/shell/create.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
-network_name=$1
-counter=$2
-port=$3
+cd ../../..
 
-# Setup couchdb
-cd couchdb
-docker run -d --name realcouchdb${counter} --network ${network_name} -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=adminpw couchdb:latest
+mvn clean package
+cp target/couchdb_logger-1.0.0.war src/main/compose/logger.war
+cd src/main/compose
+docker-compose build
+docker-compose up -d
 
-# Setup tomcat
-docker run -d --name couchdb${counter} --network ${network_name} -e COUCHDB_HOST=http://realcouchdb${counter}:5984 tomcat:9.0.53
-
-
-sleep 10
-
-# init couchdb
-docker cp ../couchdb/create-database.sh realcouchdb${counter}:/
-docker exec realcouchdb${counter} /bin/bash /create-database.sh
-
-# init tomcat
-docker cp ../../../target/couchdb_logger-1.0.0.war couchdb${0}:/var/lib/tomcat9/webapps/ROOT.war
-docker restart couchdb${0}
