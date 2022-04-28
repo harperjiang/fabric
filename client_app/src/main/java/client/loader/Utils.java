0cdc226378114da3425c3661d6d9b269311898ee
harper <harperjiang@msn.com>
2022-04-28 20:39:21 +0000

    Close connection after myoperations

---
+++ client_app/src/main/java/client/loader/Utils.java
@@ -0,0 +1,21 @@
+package client.loader;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.text.MessageFormat;
+import java.util.Random;
+
+public class Utils {
+
+    static Random random = new Random(System.currentTimeMillis());
+
+    public static String randomPrice() {
+        return String.valueOf(random.nextInt(10000));
+    }
+
+    public static String randomDate() {
+        return MessageFormat.format("{0}-{1}-{2}", String.valueOf(2000 + random.nextInt(25)),
+                StringUtils.leftPad(String.valueOf(random.nextInt(12)), 2, '0'),
+                StringUtils.leftPad(String.valueOf(random.nextInt(30)), 2, '0'));
+    }
+}
