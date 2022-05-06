142f64161679838b3572b82f194d359b0f5d1558
Ubuntu <harper@fabric1.nmes3urj0jaedlfldcn4ay4g3f.bx.internal.cloudapp.net>
2022-05-06 09:57:58 +0000

    Update and Commit

--- couchdb_logger/src/main/shell/create_embed.sh
+++ couchdb_logger/src/main/shell/create_embed.sh
@@ -3,7 +3,7 @@
 cd ../../..
 
 mvn clean package
-cp target/couchdb_logger-1.0.0-with-dependencies.jar src/main/compose/embed/logger.jar
+cp target/couchdb_logger-1.0.0-jar-with-dependencies.jar src/main/compose/embed/logger.jar
 cd src/main/compose/embed/
 docker-compose build
 docker-compose up -d
