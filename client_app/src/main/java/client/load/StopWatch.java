45b17c0ea32bbeb238611790203b7424b1db75ae
Hao Jiang <harperjiang@msn.com>
2022-05-12 23:31:24 -0400

    loggers

--- client_app/src/main/java/client/load/StopWatch.java
+++ client_app/src/main/java/client/load/StopWatch.java
@@ -1,14 +1,32 @@
 package client.load;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StopWatch {
+    private List<Record> records = new ArrayList<>();
 
+    private String currentName;
+
+    private long startTime;
     public void start(String name) {
-
+        this.currentName = name;
+        this.startTime = System.currentTimeMillis();
     }
 
     public void record() {
 
     }
 
+    static class Record {
+        long start;
+        long elapse;
+        String name;
+
+        public Record(String name) {
+            this.start = System.currentTimeMillis();
+            this.name = name;
+        }
+    }
 
 }
