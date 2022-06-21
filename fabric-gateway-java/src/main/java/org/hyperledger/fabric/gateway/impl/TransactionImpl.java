e812a18410fbcb9caa3c9bea90eb2b2f6ac2c0f7
harper <harperjiang@msn.com>
2022-06-21 08:48:55 -0400

    gradle build

--- fabric-gateway-java/src/main/java/org/hyperledger/fabric/gateway/impl/TransactionImpl.java
+++ fabric-gateway-java/src/main/java/org/hyperledger/fabric/gateway/impl/TransactionImpl.java
@@ -14,6 +14,7 @@ import java.util.concurrent.TimeoutException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.spi.LoggerFactory;
 import org.hyperledger.fabric.gateway.ContractException;
 import org.hyperledger.fabric.gateway.GatewayRuntimeException;
 import org.hyperledger.fabric.gateway.Transaction;
@@ -103,8 +104,16 @@ public final class TransactionImpl implements Transaction {
 
     @Override
     public byte[] submit(final String... args) throws ContractException, TimeoutException, InterruptedException {
+        long start = System.currentTimeMillis();
         Collection<ProposalResponse> proposalResponses = endorseTransaction(args);
+        long point1 = System.currentTimeMillis();
         Collection<ProposalResponse> validResponses = validatePeerResponses(proposalResponses);
+        long point2 = System.currentTimeMillis();
+
+        if (LOG.isInfoEnabled()) {
+            LOG.info(String.format("Proposal Time:%d", point1 - start));
+            LOG.info(String.format("Validate Time:%d", point2 - point1));
+        }
 
         try {
             return commitTransaction(validResponses);
